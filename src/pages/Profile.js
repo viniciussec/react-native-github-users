ca96655e04ed5efdbabc4a622c3abd191fe70d41
viniciussec <viniciussec@hotmail.com>
2020-09-28 22:22:54 -0300

    new info page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,9 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
+import { StyleSheet } from 'react-native'
 import { View, Text, Image } from 'react-native'
 import api from '../services/api'
 
-function Profile({navigation}) {
+function Profile({ navigation }) {
     const [userInfo, setUserInfo] = useState([])
     const userLogin = navigation.getParam('username')
 
@@ -15,15 +16,58 @@ function Profile({navigation}) {
         loadUserInfo()
     }, [])
 
-    return(
+    return (
         <View style={styles.screen}>
-            <Text>{userInfo.login}</Text>
+            <View style={styles.header}>
+                <Image style={styles.avatar} source={{ uri: userInfo.avatar_url }}></Image>
+                <Text style={styles.login}>{userInfo.login}</Text>
+            </View>
+            <Text style={styles.infoHeader}>User Info:</Text>
+            <Text style={styles.info}>Name: {userInfo.name}</Text>
+            <Text style={styles.info}>Bio: {userInfo.bio}</Text>
+            <Text style={styles.info}>Location: {userInfo.name}</Text>
+            <Text style={styles.info}>Blog: {userInfo.blog}</Text>
+            <Text style={styles.info}>Followers: {userInfo.followers}</Text>
+            <Text style={styles.info}>Public Repositories: {userInfo.public_repos}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-
+    screen: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: '#3B5998'
+    },
+    login: {
+        fontSize: 25,
+        marginLeft: 50,
+        color: '#FFF',
+        fontWeight: 'bold'
+    },
+    avatar: {
+        borderRadius: 50,
+        height: 100,
+        width: 100
+    },
+    header: {
+        margin: 10,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    info: {
+        marginTop: 10,
+        fontSize: 20,
+        color: '#FFF',
+        fontWeight: 'bold'
+    },
+    infoHeader: {
+        color: '#AAA',
+        fontSize: 24
+    }
 })
 
 export default Profile
